f610a89a450703de08b68701bcbd03672ff24676
MikaelHepner <101403839+Derwinesmitch@users.noreply.github.com>
2025-01-28 22:35:13 +0100

    Added addpage import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './pages/Login.tsx'
+import AddPage from './pages/Add.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Login />
+    <AddPage />
   </StrictMode>,
 )
