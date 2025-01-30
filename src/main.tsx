215acd27867b226b8d20cdc3416d0d1370259e82
MikaelHepner <101403839+Derwinesmitch@users.noreply.github.com>
2025-01-30 23:14:28 +0100

    Added FrontPage import and component

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './pages/Login.tsx'
 import AddPage from './pages/Add.tsx'
+import FrontPage from './pages/FrontPage.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Login />
     <AddPage />
+    <FrontPage />
   </StrictMode>,
 )
