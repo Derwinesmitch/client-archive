bad3e33b141d0557e18591e00acd0c5a05f24eec
MikaelHepner <101403839+Derwinesmitch@users.noreply.github.com>
2025-01-31 21:33:56 +0100

    Added search bar

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -9,8 +9,24 @@ export default function FrontPage() {
     };
 
     return (
-        <div>
-           <p>Search bar</p>
+        <div className="flex min-h-screen items-center justify-center bg-gray-100 px-6">
+            <div className="w-full max-w-xl text-center">
+                <h1 className="text-3x1 font-bold text-gray-900 bm-6">Welcome</h1>
+                <form onSubmit={handleSearch} className="relative">
+                    <input
+                        type="text"
+                        placeholder="search:"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        className="w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                    <button
+                        type="submit"
+                        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-md bg-indigo-600 px-4 py-2 text-white shadow md hover:bg-indigo-500">
+                            🔍
+                        </button>
+                </form>
+            </div>
         </div>
     )
 
