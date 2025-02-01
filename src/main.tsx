6a45e14f8d0a8cc2667220000c41416d1a9f682a
MikaelHepner <101403839+Derwinesmitch@users.noreply.github.com>
2025-02-01 14:27:15 +0100

    Added routing, added archive

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import './index.css'
 import Login from './pages/Login.tsx'
 import AddPage from './pages/Add.tsx'
 import FrontPage from './pages/FrontPage.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Archive from './pages/Archive.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
-    <AddPage />
-    <FrontPage />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element ={<Login />} />
+          <Route path="/AddPage" element ={<AddPage />} />
+          <Route path="/FrontPage" element ={<FrontPage />} />
+          <Route path="/archive" element ={<Archive />} />
+
+      </Routes>
+    </BrowserRouter>   
   </StrictMode>,
+
 )
